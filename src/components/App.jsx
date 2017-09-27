446cac9aad7eb43af802b6cbc529441bcb66ec6c
taranenko <bogdan13taranenko@gmail.com>
2017-09-27 18:38:56 +0300

    add styles and slider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,19 @@ import Tabs from './Tabs';
 import Tab from './Tab';
 
 const App = (props: Object) => (
-  <Tabs selected={0}>
+  <Tabs stylize="original" selected={0}>
     <Tab label="React">React is really awesome</Tab>
     <Tab label="Ember">Ember is old</Tab>
     <Tab label="Angular">Angular is super powerfull</Tab>
+    <Tab label="Jquery">Jquery is not needed</Tab>
+    <Tab label="Polymer">Polymer is a cool thing. Maybe</Tab>
+    <Tab label="RxJS">RxJS is about reactive programming</Tab>
+    <Tab label="Elm">Elm is a part of functional programming</Tab>
+    <Tab label="Flow">Flow is for static types</Tab>
+    <Tab label="Webpack">Webpack makes our life simple</Tab>
+    <Tab label="Jest">Jest is for testing React</Tab>
+    <Tab label="Redux">Redux for managing all state</Tab>
   </Tabs>
-    );
+);
 
 export default App;
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,7 @@
 // @flow
 
-
 import React, { Component } from 'react';
+import Siema from 'siema';
 
 export default class Tabs extends Component {
 
@@ -16,10 +16,17 @@ export default class Tabs extends Component {
   constructor(props: any) {
     super(props);
 
-    this.state = { selected: this.props.selected };
+    this.state = {
+      selected: this.props.selected,
+      labelsClass: '',
+      labelClass: '',
+      activeClass: '',
+    };
 
     this.labels = this.labels.bind(this);
     this.onLabelClick = this.onLabelClick.bind(this);
+    this.changeStyle = this.changeStyle.bind(this);
+    this.setupSlider = this.setupSlider.bind(this);
   }
 
   onLabelClick(index: number) {
@@ -28,11 +35,64 @@ export default class Tabs extends Component {
     });
   }
 
+  componentDidMount() {
+    this.changeStyle();
+    this.setupSlider();
+  }
+
+  setupSlider() {
+    if (this.props.children.length > 15) {
+      new Siema({
+        selector: '.tabs__labels__slider',
+        duration: 200,
+        easing: 'ease-out',
+        perPage: {
+          320: 3,
+          460: 4,
+          620: 6,
+          800: 8,
+          1240: 11,
+        },
+        startIndex: 0,
+        draggable: true,
+        multipleDrag: true,
+        threshold: 20,
+        loop: false,
+      });
+    }
+  }
+
+  changeStyle() {
+    switch (this.props.stylize) {
+      case ('original'):
+        this.setState({
+          labelClass: 'original__tabs__labels-item',
+          labelsClass: 'original__tabs__labels',
+          activeClass: 'original__is-active',
+        });
+        break;
+      case ('flat'):
+        this.setState({
+          labelClass: 'flat__tabs__labels-item',
+          labelsClass: 'flat__tabs__labels',
+          activeClass: 'flat__is-active',
+        }); break;
+      case ('material'):
+        this.setState({
+          labelClass: 'material__tabs__labels-item',
+          labelsClass: 'material__tabs__labels',
+          activeClass: 'material__is-active',
+        }); break;
+
+      default: console.log('nothing is customize');
+    }
+  }
+
   labels(child: any, index: number) {
     const onClick = this.onLabelClick.bind(this, index);
-    const activeClass = (this.state.selected === index ? 'is-active' : '');
+    const activeClass = (this.state.selected === index ? this.state.activeClass : '');
     return (
-      <li key={index} className={`${activeClass} tabs__labels-item`}>
+      <li key={index} className={`${activeClass} ${this.state.labelClass}`}>
         <a onClick={onClick}>
           {child.props.label}
         </a>
@@ -43,7 +103,7 @@ export default class Tabs extends Component {
   render() {
     return (
       <div className="tabs">
-        <ul className="tabs__labels">
+        <ul className={`${this.state.labelsClass} tabs__labels__slider`}>
           {this.props.children.map(this.labels)}
         </ul>
         <div className="tabs__content">
